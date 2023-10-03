fecffd6ffbedf7d5e87b30c0555bbe5a3925ef0f
peiq <88368889+nessqing@users.noreply.github.com>
2023-10-03 23:49:20 +0800

    update version
    1.update version
    2.try scrapying  the url info

--- selenium/helloSelenium.py
+++ selenium/helloSelenium.py
@@ -1,13 +1,25 @@
 from selenium import webdriver
-import time #for sleep to observe chrome live
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
+#for sleep to observe chrome live
 
 
 print("imlive")
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.google.com")
-
+driver.get("https://newhouse.591.com.tw/housing-list-6.html")
+# totals = driver.find_element(By.XPATH, './/span[@class = "fc-red"]').text
+# totals = driver.find_element(By.XPATH, "//span[@class='fc-red' and @id='total-build']")
+# totals = driver.find_element(By.xpath("//span[@class='fc-red' and @id='total-build']"
+# //h2[contains(@class, 'jobTitle')]/span[@title]")
+# totals = driver.find_element("total-build")
+# titles = driver.find_element(By.CLASS_NAME,'fc-red')
+# print(WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='listMenu_right']/span"))).get_attribute("innerHTML"))
+# print(titles.text)
+# for title in titles:
+#     print(title.text)
 print(driver.title)
 
 time.sleep(10) # as import time statement
