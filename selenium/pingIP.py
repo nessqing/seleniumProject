3d0f20e090814e25cb97b2512e417223db8e8f48
peiq <88368889+nessqing@users.noreply.github.com>
2023-10-12 00:19:30 +0800

    ping init
    wonder using ping to generate test profile

--- selenium/pingIP.py
+++ selenium/pingIP.py
@@ -1,14 +1,34 @@
 import os
-hostnames=['192.168.0.169','192.168.0.168']
+hostnames = []
+for i in range (51,256,1) :
+    print(i)
+    hostnames.append('192.168.119.' + str(i))
+print(hostnames)
 
+textfile = open('/Users/haru/Desktop/seleniumProject/selenium/profile.properties', "w")
 for host in hostnames :
-    response = os.system('ping -c 1 ' + host)
-    if response == 0:
-        print(host, 'is up')
-        textfile = open('/Users/haru/Desktop/seleniumProject/selenium/profile.properties',"w")
-        pa = "{}\n".format(host.split('.',4))
-        print(pa)
-        textfile.write(pa)
-        textfile.close()
-    else:
-        print(host,'is down')
+    textfile.write(host + '\n')
+textfile.close()
+# textfile.write(hostnames)
+
+# textfile = open('/Users/haru/Desktop/seleniumProject/selenium/profile.properties', "w")
+#
+# for host in hostnames :
+#     textfile.write(host)
+#     print(host)
+#
+# textfile.close()
+    # textfile = open('/Users/haru/Desktop/seleniumProject/selenium/profile.properties', "w")
+    # textfile.writelines(host)
+    # textfile.close()
+    # response = os.system('ping -c 1 ' + host)
+    # if response == 0:
+    #     print(host, 'is up')
+    #     textfile = open('/Users/haru/Desktop/seleniumProject/selenium/profile.properties',"w")
+    #     # pa = host
+    #     pa = "{}\n".format(host.split('.',4))
+    #     print(pa)
+    #     textfile.write(pa)
+    #     textfile.close()
+    # else:
+    #     print(host,'is down')
