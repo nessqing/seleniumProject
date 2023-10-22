066f4c94e5d909a44f96003951ff6bbaf65b4f1d
peiq <88368889+nessqing@users.noreply.github.com>
2023-10-22 22:51:05 +0800

    update tool
    update
    1. insert ip in flexible rows

--- pandas/helloPandas.py
+++ pandas/helloPandas.py
@@ -1,19 +1,30 @@
 import pandas as harupandas
 
-path = ('/Users/haru/Desktop/smart_port00_2023DS2W0512G013.csv')
+path = ('/Users/haru/Desktop/pandaIP/smart_port00_2023DS2W0512G013.csv')
 
-df = harupandas.read_csv(path, index_col = 0)
-harupandas.options.display.max_columns = None
-# harupandas.options.display.max_rows = None
-# print(df)
+df = harupandas.read_csv(path)
+print(df.shape[0])# count row in csv
+IP = str(path.split('/').__getitem__(-2))
 passIP =[]
 
-for i in str(df.shape[0]) :
-    passIP.append(i)
-df.insert(0,"IP",[192,194],True)
+for i in range (df.shape[0]) :
+    passIP.append(IP)
+
+print(passIP)
+print(IP)# ok for insert last folder path
+df.insert(0,"IP",passIP)
+df.to_csv('/Users/haru/Desktop/pandaexample1.csv')
+
+
+
+
+
+
+# harupandas.options.display.max_columns = None
+# harupandas.options.display.max_rows = None
+# print(df)
 # print(df.shape[0])
 # harupandas.options.display.max_columns = None
 # harupandas.options.display.max_rows = None
 # print(dataframe)
-df.to_csv('/Users/haru/Desktop/pandaexample1.csv')
 # print(df.tail(1))
